15d404f48acebcb8e87d53029a0564f5162264e5
Steven Garis <steven.t.garis@gmail.com>
2019-04-01 16:44:03 -0500

    adding model

--- classification/acquire.py
+++ classification/acquire.py
@@ -1,12 +1,12 @@
-import env
+    
 import pandas as pd
+import env
 
 def get_connection(db, user=env.user, host=env.host, password=env.password):
-   return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
 def get_titanic_data():
-   return pd.read_sql('SELECT * FROM passengers', get_connection('titanic_db'))
+    return pd.read_sql('SELECT * FROM passengers', get_connection('titanic_db'))
 
 def get_iris_data():
-   return pd.read_sql('SELECT s.species_name, m.* FROM species s JOIN measurements m ON m.species_id = s.species_id', get_connection('iris_db'))
-
+    return pd.read_sql('SELECT m.*, s.species_name FROM measurements m JOIN species s USING(species_id)', get_connection('iris_db'))
--- classification/prepare.py
+++ classification/prepare.py
@@ -1,25 +1,34 @@
+from pandas import DataFrame
+from sklearn.preprocessing import LabelEncoder
 from sklearn import preprocessing
 
 
+def handle_missing_values(df):
+    return df.assign(
+        embark_town=df.embark_town.fillna('Other'),
+        embarked=df.embarked.fillna('O'),
+    )
+
+def remove_columns(df):
+    return df.drop(columns=['deck'])
+
+def encode_embarked(df):
+    encoder = LabelEncoder()
+    encoder.fit(df.embarked)
+    return df.assign(embarked_encode = encoder.transform(df.embarked))
+
+def prep_titanic_data(df):
+    df = df\
+        .pipe(handle_missing_values)\
+        .pipe(remove_columns)\
+        .pipe(encode_embarked)
+    return df
+
+
 def prep_iris(df_iris):
-   df_iris.drop(['species_id', 'measurement_id'], axis=1, inplace=True)
-   df_iris.rename(columns={'species_name': 'species'}, inplace=True)
-   le = preprocessing.LabelEncoder()
-   le.fit(df_iris['species'])
-   df_iris['species_encoded'] = le.transform(df_iris['species'])
-   return df_iris
-
-
-def prep_titanic(df_titanic):
-   df_titanic.embark_town.fillna('Other', inplace=True)
-   df_titanic.embarked.fillna('O', inplace=True)
-   df_titanic.drop(columns='deck', inplace=True)
-   le_titanic = preprocessing.LabelEncoder()
-   le_titanic.fit(df_titanic['embarked'])
-   df_titanic['embarked'] = le_titanic.transform(df_titanic['embarked'])
-   train, test = train_test_split(df_titanic, train_size=.7)
-   mm_scaler = preprocessing.MinMaxScaler()
-   mm_scaler.fit(train[['age', 'fare']])
-   train[['age', 'fare']] = mm_scaler.transform(train[['age', 'fare']])
-   test[['age', 'fare']] = mm_scaler.transform(test[['age', 'fare']])
-   return df, train, test
+    df_iris.drop(['species_id', 'measurement_id'], axis=1, inplace=True)
+    df_iris.rename(columns={'species_name': 'species'}, inplace=True)
+    le = preprocessing.LabelEncoder()
+    le.fit(df_iris['species'])
+    df_iris['species_encoded'] = le.transform(df_iris['species'])
+    return df_iris
